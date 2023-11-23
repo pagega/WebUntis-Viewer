35ebe7ba090fc56ebe22a8ddb87c0796b370fdc4
pagega <73445837+pagega@users.noreply.github.com>
2023-11-23 10:28:30 +0100

    Create Viewer.py
    - Code in Viewer.py kopiert.
    - Rep erstellt

---
+++ Viewer.py
@@ -0,0 +1,238 @@
+import requests
+from tabulate import tabulate
+from tkinter import *
+from datetime import datetime, timedelta
+from tkinter import ttk
+
+class DATA:
+    @classmethod
+    def initialize(cls, date=None):
+        try:
+            if date is None:
+                selected_date_str = GUI.datum.get()
+                parsed_date = datetime.strptime(selected_date_str, "%A, %d.%m.%Y")
+                date = parsed_date.strftime("%Y%m%d")
+                
+        except Exception:
+            date = datetime.now().strftime("%Y%m%d")
+
+        cls.daten = cls.daten_von_webuntis(date)
+        
+        cls.vertretungen = cls.daten['payload']['rows']
+        cls.keys = cls.daten['payload']['affectedElements']['1']
+        cls.sort_data = cls.daten_bereinigen(cls.vertretungen, cls.keys)
+
+
+    @staticmethod
+    def daten_von_webuntis(selected_date_str):
+        
+        try:
+            date = int(selected_date_str)
+        except Exception:
+            parsed_date = datetime.strptime(selected_date_str, "%A, %d.%m.%Y")
+            date = parsed_date.strftime("%Y%m%d")
+        
+        cookies = {
+            'traceId': 'c6d915932d178a01b45014d09ed504ce9c790385',
+            'schoolname': '"_YmJzIGZyaWVzb3l0aGU="',
+            'traceId': 'c6d915932d178a01b45014d09ed504ce9c790385',
+            'schoolname': '"_YmJzIGZyaWVzb3l0aGU="',
+            'JSESSIONID': 'C16CF01D56D67A725BD1F196F4CB1CEA',
+        }
+
+        headers = {
+            'authority': 'kephiso.webuntis.com',
+            'accept': '*/*',
+            'accept-language': 'de-DE,de;q=0.9,en-DE;q=0.8,en;q=0.7,es-ES;q=0.6,es;q=0.5,en-US;q=0.4',
+            'content-type': 'application/json',
+            'dnt': '1',
+            'origin': 'https://kephiso.webuntis.com',
+            'referer': 'https://kephiso.webuntis.com/WebUntis/monitor?school=BBS%20Friesoythe&monitorType=subst&format=Vertretung%20heute',
+            'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
+        }
+
+        params = {
+            'school': 'BBS Friesoythe',
+        }
+
+        json_data = {
+            'formatName': 'Vertretung morgen',
+            'schoolName': 'BBS Friesoythe',
+            'date': date,
+            'dateOffset': 0,
+            'strikethrough': True,
+            'mergeBlocks': True,
+            'showOnlyFutureSub': True,
+            'showBreakSupervisions': False,
+            'showTeacher': True,
+            'showClass': False,
+            'showHour': True,
+            'showInfo': True,
+            'showRoom': True,
+            'showSubject': False,
+            'groupBy': 1,
+            'hideAbsent': True,
+            'departmentIds': [],
+            'departmentElementType': -1,
+            'hideCancelWithSubstitution': True,
+            'hideCancelCausedByEvent': False,
+            'showTime': False,
+            'showSubstText': True,
+            'showAbsentElements': [1, 2],
+            'showAffectedElements': [1, 2],
+            'showUnitTime': True,
+            'showMessages': True,
+            'showStudentgroup': False,
+            'enableSubstitutionFrom': True,
+            'showSubstitutionFrom': 1700,
+            'showTeacherOnEvent': False,
+            'showAbsentTeacher': True,
+            'strikethroughAbsentTeacher': True,
+            'activityTypeIds': [],
+            'showEvent': False,
+            'showCancel': True,
+            'showOnlyCancel': False,
+            'showSubstTypeColor': False,
+            'showExamSupervision': False,
+            'showUnheraldedExams': False,
+        }
+
+        response = requests.post(
+            'https://kephiso.webuntis.com/WebUntis/monitor/substitution/data',
+            params=params,
+            cookies=cookies,
+            headers=headers,
+            json=json_data,
+        )
+        
+        return response.json()
+
+    @staticmethod
+    def daten_bereinigen(Daten, Keys):
+        
+        Daten_kurz = {}
+        
+        for klassen in Keys:
+            Daten_kurz[klassen] = []
+            
+        for eintrag in Daten:
+            eintrag["data"].insert(0, eintrag["group"])
+            for i in range(len(eintrag["data"])):
+                eintrag["data"][i] = eintrag["data"][i].replace('<span class="substMonitorSubstElem">', "").replace('</span>', "").replace('<span class="cancelStyle">', '').replace('Raum&auml;nderung', 'Raumänderung')
+            Daten_kurz[eintrag["data"][0]].append(eintrag['data'])
+            
+        return Daten_kurz
+
+    @staticmethod
+    def mögliche_tage():
+       
+        # Funktion, um Samstag und Sonntag zu überspringen
+        def naechster_werktag(datum):
+            while True:
+                datum += timedelta(days=1)
+                if datum.weekday() < 5:  # 0-4 sind Montag bis Freitag
+                    return datum
+
+        # Aktuelles Datum holen
+        heutiges_datum = datetime.now()
+        naechste_tage = []
+        
+        if heutiges_datum.weekday() != 5 and heutiges_datum.weekday() != 6:
+            naechste_tage.append(f'{heutiges_datum.strftime('%A')}, {heutiges_datum.day}.{heutiges_datum.month}.{heutiges_datum.year}')
+            tage = 13
+        else:
+            tage = 14
+            
+        # Liste für die nächsten 5 Werktage erstellen
+        
+        for _ in range(tage):
+            heutiges_datum = naechster_werktag(heutiges_datum)
+            naechste_tage.append(f'{heutiges_datum.strftime('%A')}, {heutiges_datum.day}.{heutiges_datum.month}.{heutiges_datum.year}')
+             
+        return naechste_tage
+
+# Create the GUI
+class GUI:
+    
+    def __init__(self, master):
+        
+        self.selected_date = StringVar()
+        
+        self.master = master
+        self.klasse = StringVar(master)
+        self.klasse.set(DATA.keys[0])
+
+        self.datum = StringVar(master)
+        self.datum.set(DATA.mögliche_tage()[0])
+
+        self.klasse.trace_add('write', self.update_data)
+        self.datum.trace_add('write', self.update_data)
+
+        self.klasse_menu = OptionMenu(master, self.klasse, *DATA.keys)
+        self.datum_menu = OptionMenu(master, self.datum, *DATA.mögliche_tage())
+
+        self.klasse_menu.grid(row=1, column=0)
+        self.datum_menu.grid(row=1, column=1)
+
+        # Tabelle erstellen
+        self.table = ttk.Treeview(master, columns=(1, 2, 3, 4, 5), show="headings")
+
+        self.table_headings = [
+                'Stunde',
+                'Raum',
+                'Lehrer',
+                'Info',
+                'Vertretungstext'
+        ]
+        # Überschriften hinzufügen
+        for i in range(1, 6):
+            self.table.heading(i, text=self.table_headings[i - 1])
+
+        # Tabelle anzeigen
+        self.table.grid(row=2, column=0, columnspan=2)
+
+        # Initial update of the table
+        self.update_table()
+        
+    def update_klasse_menu(self):
+        new_keys = DATA.keys
+        menu = self.klasse_menu['menu']
+        menu.delete(0, 'end')  # Remove old options
+
+        for key in new_keys:
+            menu.add_command(label=key, command=lambda value=key: self.klasse.set(value))
+        if new_keys:
+            self.klasse.set(new_keys[0])
+
+
+    def update_table(self, *args):
+        
+        self.update_klasse_menu()
+        self.table.delete(*self.table.get_children())
+        selected_klasse = self.klasse.get()
+        for row in DATA.sort_data[selected_klasse]:
+            row.pop(0)
+            self.table.insert("", "end", values=row)
+
+    def update_data(self, *args):
+        selected_date = self.datum.get()
+        DATA.initialize(selected_date)
+        self.update_table()
+
+        
+# Initialize the data after the GUI is set up
+DATA.initialize()
+
+# Create the main Tkinter window
+root = Tk()
+# Create an instance of the GUI class
+gui = GUI(root)
+# Start the Tkinter event loop
+root.mainloop()
